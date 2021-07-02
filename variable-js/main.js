2018852a5fef81f0b623fdd3e2d3cbaecd81ba60
sergiorios777 <sergio.rios777@gmail.com>
2021-07-02 12:36:39 -0500

    Ampliación variables

--- variable-js/main.js
+++ variable-js/main.js
@@ -1,9 +1,28 @@
 const buttonA = document.querySelector('#button_A');
 const headingA = document.querySelector('#heading_A');
 
+const buttonB = document.querySelector('#button_B');
+const section = document.querySelector('#heading_B');
+
 buttonA.onclick = function ()
 {
     let name = prompt('¿Cuál es tu nombre?');
-    alert('¡Hola, ' + name + ' un guste de verte!');
+    alert('¡Hola, ' + name + ', que agradable de verte!');
     headingA.textContent = 'Bienvenido, ' + name;
+}
+
+buttonB.onclick = function ()
+{
+    let myName;
+    myName = 'Pepito';
+
+    let myAge = 42;
+
+    section.innerHTML = ' ';
+    let para1 = document.createElement('p');
+    para1.textContent = myName;
+    let para2 = document.createElement('p');
+    para2.textContent = myAge;
+    section.appendChild(para1);
+    section.appendChild(para2);
 }
